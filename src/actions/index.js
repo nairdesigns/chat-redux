29450b954845c93b077c53f08bcbb7cccc4d09da
nairdesigns <nairbdg@gmail.com>
2022-04-20 16:49:09 +0800

    errors but going somewhere added index.js functions fetch messages

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,11 @@
 // TODO: add and export your own actions
-const initialState = {
-  messages: [
-    {
-      author: "anonymous92",
-      content: "Hello world!",
-      created_at: "2017-09-26T23:03:16.365Z",
-    },
-    {
-      author: "anonymous77",
-      content: "My name is anonymous77",
-      created_at: "2017-09-26T23:03:21.194Z",
-    },
-  ],
-};
+
+export function fetchMessages(channel) {
+  const url = `${BASE_URL}/${channel}/messages`;
+  const promise = fetch(url).then((r) => r.json());
+
+  return {
+    type: FETCH_MESSAGES,
+    payload: promise, // Will be resolved by redux-promise
+  };
+}
