c950e36f3c45cf34eed17293885bc5f1b4325107
nairdesigns <nairbdg@gmail.com>
2022-04-20 16:44:20 +0800

    ERROR added message list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import MessageList from "../containers/message_list";
 const App = () => {
   return (
     <div className="app">
       <p>React ddddddd+ Redux starter</p>
+      <MessageList />
     </div>
   );
 };
