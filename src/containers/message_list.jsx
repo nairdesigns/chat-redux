5b4a00bf8eef1ae0b97e240651343d5e34454754
nairdesigns <nairbdg@gmail.com>
2022-04-20 18:50:31 +0800

    redid to old version

---
+++ src/containers/message_list.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { fetchMessages } from "../actions";
+
+class MessageList extends Component {
+  componentWillMount() {
+    this.fetchMessages();
+  }
+
+  componentDidMount() {
+    this.refresher = setInterval(this.fetchMessages, 5000);
+  }
+
+  componentDidUpdate() {
+    this.list.scrollTop = this.list.scrollHeight;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refresher);
+  }
+
+  fetchMessages = () => {
+    this.props.fetchMessages(this.props.selectedChannel);
+  };
+  render() {
+    return (
+      <div className="channel-container">
+        <div className="channel-title">
+          <span>Channel #{this.props.selectedChannel}</span>
+        </div>
+        <div
+          className="channel-content"
+          ref={(list) => {
+            this.list = list;
+          }}
+        >
+          {/* {this.props.messages.map((message) => {
+            return <Message key={message.id} message={message} />;
+          })} */}
+        </div>
+        {/* <MessageForm /> */}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    message: state.message,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMessages }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
