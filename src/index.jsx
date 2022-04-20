8089b33b65ffc674028495b4026de8923a1de5ae
nairdesigns <nairbdg@gmail.com>
2022-04-20 20:20:48 +0800

    WORKNG! progbem was soemwhere in index.jsx line 38

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
+/* eslint no-alert:off */
+
 // external modules
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
 import logger from "redux-logger";
+import reduxPromise from "redux-promise";
 
 // internal modules
 import App from "./components/app";
@@ -14,34 +16,32 @@ import "../assets/stylesheets/application.scss";
 import messagesReducer from "./reducers/messages_reducer";
 import selectedChannelReducer from "./reducers/selected_channel_reducer";
 
+const identityReducer = (state = null) => state;
+
+const initialState = {
+  messages: [],
+  channels: ["general", "react", "paris"],
+  currentUser:
+    prompt("What is your username?") ||
+    `anonymous${Math.floor(10 + Math.random() * 90)}`,
+  selectedChannel: "general",
+};
+
 const reducers = combineReducers({
   messages: messagesReducer,
-  // channels: identityReducer,
-  // currentUser: identityReducer,
+  channels: identityReducer,
+  currentUser: identityReducer,
   selectedChannel: selectedChannelReducer,
 });
 
-const initialState = {
-  messages: [
-    {
-      author: "anonymous92",
-      content: "Hello world!",
-      created_at: "2017-09-26T23:03:16.365Z",
-    },
-    {
-      author: "anonymous77",
-      content: "My name is anonymous77",
-      created_at: "2017-09-26T23:03:21.194Z",
-    },
-  ],
-};
+// Middlewares
 const middlewares = applyMiddleware(reduxPromise, logger);
-const store = createStore(reducers, initialState);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById("app")
 );
