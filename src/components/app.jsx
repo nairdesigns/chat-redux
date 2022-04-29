5269ab10ba8044113639e85d17a366a291b3350e
nairdesigns <nairbdg@gmail.com>
2022-04-29 19:17:15 +0800

    logo finallly loaded

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import MessageList from "../containers/message_list";
 import ChannelList from "../containers/channel_list";
 
@@ -6,7 +7,13 @@ const App = () => {
   return (
     <div className="app">
       <div className="row">
-        <div className="col-1 ">put logo here</div>
+        <div className="col-1 ">
+          <img
+            src="https://uikit.lewagon.com/assets/logo-57f7cc2a977ba6dae9243c48b9469d5b6e588a2654f7b16152f871ced54b57f2.png"
+            alt="logo"
+          />
+        </div>
+
         <ChannelList />
         <MessageList />
       </div>
